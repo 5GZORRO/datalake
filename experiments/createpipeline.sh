9eba1316181ba74fd96a54fe5e5ed4b7647f96db
Kalman Meth <meth@il.ibm.com>
2021-03-17 14:47:56 +0200

    passed user id in createpipeline

--- experiments/createpipeline.sh
+++ experiments/createpipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 tmpfile=$(mktemp)
 echo "tmpfile = " $tmpfile
-echo ' { "userInfo": { "userId": "user2", "authToken": "blah" }, "pipelineDefinition": '  >$tmpfile
-cat $1 >>$tmpfile
+echo ' { "userInfo": { "userId": '\"$1\"', "authToken": "blah" }, "pipelineDefinition": '  >$tmpfile
+cat $2 >>$tmpfile
 echo '}' >>$tmpfile
 curl -i -H "Content-Type: application/json" -X POST --data "@$tmpfile"  localhost:8080/datalake/v1/pipeline
 rm $tmpfile
