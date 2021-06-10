caac0043eb25b3ec025e9444de7a03786abe89ef
Kalman Meth <meth@il.ibm.com>
2021-06-10 14:02:20 +0300

    specified expected inputs to getresourcemetrics.sh

--- services/catalog/getresourcemetrics.sh
+++ services/catalog/getresourcemetrics.sh
@@ -1,2 +1,4 @@
 set -x
+# Usage: getresourcemetrics.sh <user-id> <resource-id> <catalog-server-url>
+# The catalog-server-url was returned in availableResources in the registerUser API
 curl -i -H "Content-Type: application/json" -X GET -d ' { "userId": '\"$1\"', "authToken": "blah" } ' $3/datalake/v1/catalog/resource/$2
