4d1b34a837ddd2a33315941c91254fb34e59021d
Kalman Meth <meth@il.ibm.com>
2021-07-08 10:47:24 +0300

    placed predefined pipelines in separate file

--- python-flask-server/swagger_server/controllers/user_controller.py
+++ python-flask-server/swagger_server/controllers/user_controller.py
@@ -1,12 +1,9 @@
 import connexion
 import six
-import os
 
 from flask import Response
 from swagger_server.models.user import User  # noqa: E501
 from swagger_server.models.user_resources import UserResources  # noqa: E501
-from swagger_server.models.pipeline_metadata import PipelineMetadata
-from swagger_server.models.pipeline_info import PipelineInfo
 from swagger_server import util
 from swagger_server.controllers.user_info import UserInfo
 from swagger_server.controllers import user_info
@@ -15,6 +12,7 @@ from swagger_server.controllers import s3_api
 from swagger_server.controllers import kafka_api
 from swagger_server.controllers import pipeline_controller
 from swagger_server.controllers import service_controller
+from swagger_server.controllers import predefined_pipelines
 from swagger_server.controllers import dl_global_services
 
 def get_user():  # noqa: E501
@@ -57,134 +55,6 @@ def list_users():  # noqa: E501
     :rtype: List[str]
     """
     return list(user_info.get_users())
-
-#TODO separate this out into a separate file
-def create_predefined_pipelines(user_id, s3_available : bool):
-    print("entering create_predefined_pipelines")
-    predefined_pipes = list()
-    pipeline_topics = { }
-    if not s3_available:
-        return pipeline_topics, predefined_pipes
-    # create default ingest metrics pipeline
-    # TODO: perhaps move this to a config file yaml and have a more general mechanism to add predefined pipelines
-    datalake_images_version = os.getenv('DATALAKE_IMAGES_VERSION', '1.0')
-    ingest_def = {
-        "apiVersion": "argoproj.io/v1alpha1",
-        "kind": "Workflow",
-        "metadata": {
-            "generateName": "ingest-pipeline-"
-        },
-        "spec": {
-            "entrypoint": "ingest-and-index",
-            "imagePullSecrets": [
-                { "name": "datalakeregistrykey" }
-            ],
-            "arguments": {
-                "parameters": [ {
-                    "name": "args",
-                    "value": "my args"
-                } ]
-            },
-            "templates": [
-              {
-                "name": "ingest-and-index",
-                "inputs": {
-                    "parameters": [ {
-                        "name": "args"
-                    } ]
-                },
-                "steps": [
-                    [ {
-                        "name": "ingest1",
-                        "template": "ingest",
-                        "arguments": {
-                            "parameters": [ {
-                                "name": "args",
-                                "value": "{{inputs.parameters.args}}"
-                            } ]
-                        }
-                    } ],
-                    [ {
-                        "name": "index1",
-                        "template": "metrics-index",
-                        "arguments": {
-                            "parameters": [ {
-                                "name": "args",
-                                "value": "{{steps.ingest1.outputs.result}}"
-                            } ]
-                        }
-                    } ]
-                ]
-              },
-              {
-                "name": "ingest",
-                "inputs": {
-                    "parameters": [ {
-                        "name": "args"
-                    } ]
-                },
-                "container": {
-                    "image": "docker.pkg.github.com/5gzorro/datalake/ingest:"+datalake_images_version,
-                    "env": [
-                        { "name": "S3_URL",
-                        "value": os.getenv('S3_URL', '127.0.0.1:9000') },
-                        { "name": "S3_ACCESS_KEY",
-                        "value": os.getenv('S3_ACCESS_KEY', 'user') },
-                        { "name": "S3_SECRET_KEY",
-                        "value": os.getenv('S3_SECRET_KEY', 'password') },
-                    ],
-                    "command": [ "python", "./ingest.py" ],
-                    "args": ["{{inputs.parameters.args}}"],
-                    "resources": {
-                        "limits": {
-                            "memory": "32Mi",
-                            "cpu": "100m"
-                        }
-                    }
-                }
-              },
-              {
-                "name": "metrics-index",
-                "inputs": {
-                    "parameters": [ {
-                        "name": "args"
-                    } ]
-                },
-                "container": {
-                    "image": "docker.pkg.github.com/5gzorro/datalake/metrics_index:"+datalake_images_version,
-                    "env": [
-                        { "name": "POSTGRES_HOST",
-                        "value": os.getenv("POSTGRES_HOST", "127.0.0.1") },
-                    ],
-                    "command": [ "python", "./metrics_index.py" ],
-                    "args": ["{{inputs.parameters.args}}"],
-                    "resources": {
-                        "limits": {
-                            "memory": "32Mi",
-                            "cpu": "100m"
-                        }
-                    }
-                }
-              }
-            ]
-        }
-    }
-    k8s_proxy_server = k8s_api.get_k8s_proxy()
-    try:
-        ingest_topic, kafka_key = k8s_proxy_server.create_eventsource(user_id, 'in', pipeline_number=0)
-        print("ingest_topic = ", ingest_topic)
-        response = k8s_proxy_server.create_sensor(ingest_topic, kafka_key, ingest_def)
-        pipeline_id = response['metadata']['name']
-        pipe_metadata = PipelineMetadata(pipeline_id, ingest_topic)
-        pipe_info = PipelineInfo(pipe_metadata, ingest_def)
-        pipeline_topics["resourceMetricsIngestPipeline"] = ingest_topic
-        predefined_pipes.append(pipe_info)
-    except Exception as e:
-        print("Exception: ", str(e))
-
-    # Add here additional pipelines, as needed
-
-    return pipeline_topics, predefined_pipes
 
 def register_user(body):  # noqa: E501
     """Register a new user
@@ -230,7 +100,7 @@ def register_user(body):  # noqa: E501
                 "userInTopic": topic_name_in,
                 "userOutTopic": topic_name_out,
                 }
-        pipeline_topics, predefined_pipes = create_predefined_pipelines(user_id, s3_bucket_name != None)
+        pipeline_topics, predefined_pipes = predefined_pipelines.create_predefined_pipelines(user_id, s3_bucket_name != None)
         # TODO: make variable names consistent
         availableResources = {
                 "pipelines": pipeline_topics,
